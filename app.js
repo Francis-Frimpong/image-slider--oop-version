7de5e1bcb0dd817befe0e1808941d737b10f6ad8
Francis <brakay1136@gmail.com>
2025-08-13 11:40:41 -0700

    oop version of slider done

---
+++ app.js
@@ -0,0 +1,42 @@
+const images = [
+  "images/ninja-1.png",
+  "images/ninja-2.png",
+  "images/ninja-3.png",
+];
+
+class ImageSlider {
+  constructor(images, imgElement) {
+    this.images = images;
+    this.currentIndex = 0;
+    this.imgElement = imgElement;
+  }
+
+  showImage() {
+    imageElement.src = images[this.currentIndex];
+  }
+
+  nextImage() {
+    if (this.currentIndex < this.images.length - 1) {
+      this.currentIndex++;
+    } else {
+      this.currentIndex = 0;
+    }
+    this.showImage();
+  }
+  previousImage() {
+    if (this.currentIndex > 0) {
+      this.currentIndex--;
+    } else {
+      this.currentIndex = this.images.length - 1;
+    }
+    this.showImage();
+  }
+}
+const imageElement = document.getElementById("sliderImage");
+const nextBtn = document.getElementById("next");
+const prevBtn = document.getElementById("prev");
+const imageSlider = new ImageSlider(images, imageElement);
+nextBtn.addEventListener("click", () => imageSlider.nextImage());
+prevBtn.addEventListener("click", () => imageSlider.previousImage());
+
+imageSlider.showImage();
